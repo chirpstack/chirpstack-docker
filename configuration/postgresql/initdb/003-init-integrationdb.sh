8f4bbf118c96b27c2e700f7ba014a28f39f07674
Staņislavs <stas.jurgensons@gmail.com>
2023-12-05 13:57:11 +0200

    Set dsn and container name for postgre integration

--- configuration/postgresql/initdb/003-init-integrationdb.sh
+++ configuration/postgresql/initdb/003-init-integrationdb.sh
@@ -3,6 +3,5 @@ set -e
 
 # PostgreSQL commands to create a new role and database for ChirpStack
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create role chirpstack with login password 'chirpstack';
     create database chirpstack_integration with owner chirpstack;
 EOSQL
